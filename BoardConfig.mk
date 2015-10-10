c57af636d86020b0b81bc7f17e8bb8f40512e3fb
root <xyyx@mail.ru>
2015-10-10 12:26:33 +0800

    Insert Asserts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,9 @@ TARGET_NO_RPC := true
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/geehrc
 
+#Asserts
+TARGET_OTA_ASSERT_DEVICE := gee,geeb,geehrc,e975,geehrc_4g_spr,geespr,ls970,geehrc4g,geehrc4g_spr_us,geebus,e970,e973,e971,geeb_att_us
+
 BOARD_SEPOLICY_DIRS += \
        device/lge/geehrc/sepolicy
 
